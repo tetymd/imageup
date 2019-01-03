88928d58cf6259f26461c7332efa4e7533844a16
Tetsuya Yamada <yamada.tetsuya258@gmail.com>
2019-01-03 21:46:18 +0900

    implement restframework

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'imageup'
+    'imageup',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- api/imageup/views.py
+++ api/imageup/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from rest_framework import viewsets
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
