ee98b67397c7ade4e11d04760af6de86fe14d18e
Tetsuya Yamada <yamada.tetsuya258@gmail.com>
2019-01-03 18:16:36 +0900

    add upload.vue

---
+++ client/src/views/Upload.vue
@@ -0,0 +1,9 @@
+<template>
+    <div id="upload-form">
+        <form method="POST" action="example" target="_self">
+            <input type="file" name="image1">
+            <input type="submit" value="送信">
+        </form>
+    </div>
+</template>
+
---
+++ client/src/views/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app1">
+      {{ msg }}
+    </div>
+</template>
+
+<script>
+export default ({
+    data() {
+        return {
+            msg: "python"
+        }
+    }
+})
+</script>
