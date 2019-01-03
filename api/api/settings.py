c4bfe56d0c63caacfe6c2682369759a2425ed1d8
Tetsuya Yamada <yamada.tetsuya258@gmail.com>
2019-01-03 18:59:58 +0900

    change default DB to mysql

--- api/api/settings.py
+++ api/api/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'imageup',
+        'USER': 'imageup',
+        'PASSWORD': 'develop',
+        'HOST': '127.0.0.1'
     }
 }
 
@@ -104,15 +107,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#  LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-jp'
 
-TIME_ZONE = 'UTC'
+#  TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+#  USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
