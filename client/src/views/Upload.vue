195d0e0000f2247d7b9eba2b19acaaa9263233ca
Tetsuya Yamada <yamada.tetsuya258@gmail.com>
2019-01-05 20:01:07 +0900

    add axios

--- client/src/views/Upload.vue
+++ client/src/views/Upload.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="upload-form">
-        <form method="POST" action="example" target="_self">
-            <input type="file" name="image1">
+        <form method="POST" action="http://localhost:8000/api/thread/" target="http://localhost:8080/#/upload">
+            <input type="text" name="title"><br>
+            <textarea name="description"></textarea>
             <input type="submit" value="送信">
         </form>
     </div>
